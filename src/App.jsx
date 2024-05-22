d5944db2fc6bf0e892d0ef78640002dd8f1da2ec
Arcandres <Arcandres@hotmail.com>
2024-05-21 21:39:18 -0500

    Homepage and aboutus page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function HomePage() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home</h1>
+      <p>Homepage example</p>
+      <a href="/about">About us</a>
     </>
+  )
+}
+
+function AboutUs() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>My name is Andres and this is a simple react-router clone from scratch.</p>
+      <a href="/">Back to home</a>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <HomePage />
+      <AboutUs />
+    </main>
   )
 }
 
