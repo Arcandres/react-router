301306a5383eeae973f51a18071fc53c5a50da13
Arcandres <Arcandres@hotmail.com>
2024-05-21 22:27:05 -0500

    function navigate to move between routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+
+const EVENT = 'navigateTo'
 
 function HomePage() {
 
@@ -7,7 +10,7 @@ function HomePage() {
     <>
       <h1>Home</h1>
       <p>Homepage example</p>
-      <a href="/about">About us</a>
+      <button onClick={() => navigate('/about')}>About us</button>
     </>
   )
 }
@@ -17,16 +20,38 @@ function AboutUs() {
     <>
       <h1>About Us</h1>
       <p>My name is Andres and this is a simple react-router clone from scratch.</p>
-      <a href="/">Back to home</a>
+      <button onClick={() => navigate('/')}>Back to home</button>
     </>
   )
 }
 
+function navigate (path) {
+  window.history.pushState({}, '', path)
+
+  const navigationEvent = new Event(EVENT);
+  window.dispatchEvent(navigationEvent)
+}
+
 function App() {
+  const [actualPath, setActualPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setActualPath(window.location.pathname)
+    }
+
+    window.addEventListener(EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(EVENT, onLocationChange)
+    }
+    
+  }, [])
+
   return (
     <main>
-      <HomePage />
-      <AboutUs />
+      {actualPath === '/' && <HomePage />}
+      {actualPath === '/about' && <AboutUs />}
     </main>
   )
 }
