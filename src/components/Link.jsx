dd029d1b09070b1d1fce359240c33c8b5e9a5090
Arcandres <Arcandres@hotmail.com>
2024-05-21 23:44:27 -0500

    Managing click event with custom keys

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -9,8 +9,16 @@ export function navigateTo(path) {
 
 export function Link ({ target, to, ...props }) {
   const handleClick = (event) => {
-    event.preventDefault()
-    navigateTo(to)
+    
+    const isMainEvent = event.button === 0
+    const isModifiedEvent = event.ctrlKey || event.shiftKey
+    const isManageableEvent = target === undefined || target === '_self'
+
+    if (isMainEvent && isManageableEvent && !isModifiedEvent) {
+      event.preventDefault()
+      navigateTo(to)
+    }
+
   }
 
   return <a onClick={handleClick} href={to} target={target} {...props} />
